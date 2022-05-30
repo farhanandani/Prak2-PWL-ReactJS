e044256321be8a494d61e1bc4e5159a5c85e7bc6
farhanandani <farhanandani@gmail.com>
2022-05-31 03:52:49 +0700

    Messages Js

---
+++ src/components/Messages.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Messages = () => {
+    return (
+        <div className="card">
+            <div className="card-block">
+                <h2>Messages</h2>
+                <div className="alert alert-danger">test message</div>
+            </div>
+        </div>
+    );
+}
+
+export default Messages;
